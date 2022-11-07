3216638c964d5f8eaef863129d6ecf26ffdb5027
Oksana Ivantsiv <59508997+Olja2020@users.noreply.github.com>
2022-11-07 22:32:47 +0200

    feat: upgrade index.js file

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+ const fs=require('fs');
+ const path=require('path');
 
+
+const folderName= path.join(__dirname, 'files');
+
+
+function copyDir(folder) {
+  const copyFolderName= path.join(__dirname, 'copyFiles');
+  fs.mkdir(path.resolve(__dirname, 'copyFiles'), { recursive: true }, (err) => {
+    if (err) throw err;
+ });
+     fs.readdir(folderName, (err, files)=>{
+       if(err)throw err;
+       
+       for(let file of files){
+             const fileName= path.join(folderName, file);
+             const copyFileName= path.join(copyFolderName, file);
+             fs.copyFile(fileName, copyFileName, (err)=>{
+             if(err)throw err;
+             
+        });
+      }
+     });
+ 
+}
+copyDir(folderName);
